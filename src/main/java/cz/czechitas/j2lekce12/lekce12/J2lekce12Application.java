5a2158c6f9f801118032764a7c5b7f559fea5f77
Rinecka <cerna.kat@gmail.com>
2022-12-12 18:54:59 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/j2lekce12/lekce12/J2lekce12Application.java
@@ -0,0 +1,13 @@
+package cz.czechitas.j2lekce12.lekce12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2lekce12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2lekce12Application.class, args);
+    }
+
+}
---
+++ src/test/java/cz/czechitas/j2lekce12/lekce12/J2lekce12ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.czechitas.j2lekce12.lekce12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2lekce12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
